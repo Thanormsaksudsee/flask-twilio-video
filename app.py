6603b326d452852498ed3a81dc3216b422d3e894
Bigkuma <s6506022420037@email.kmutnb.ac.th>
2023-08-25 09:38:51 +0700

    big

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 
 load_dotenv()
-twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-twilio_api_key_sid = os.environ.get('TWILIO_API_KEY_SID')
-twilio_api_key_secret = os.environ.get('TWILIO_API_KEY_SECRET')
-twilio_client = Client(twilio_api_key_sid, twilio_api_key_secret,
-                       twilio_account_sid)
+twilio_account_sid = 'AC224ed1fdd954915e8ceb92fd6af24ef8'
+twilio_api_key_sid = 'SK837567be456422a5afff843881d420ca'
+twilio_api_key_secret = 'ndOFOCjaRP7W3pstvKtn4XQjP2PFLvuF'
+twilio_client = Client(twilio_api_key_sid, twilio_api_key_secret, twilio_account_sid)
+
 
 app = Flask(__name__)
 
@@ -48,10 +48,12 @@ def login():
     token = AccessToken(twilio_account_sid, twilio_api_key_sid,
                         twilio_api_key_secret, identity=username)
     token.add_grant(VideoGrant(room='My Room'))
-    token.add_grant(ChatGrant(service_sid=conversation.chat_service_sid))
+    token.add_grant(ChatGrant(service_sid=conversation.chat_service_sid)) 
 
-    return {'token': token.to_jwt().decode(),
-            'conversation_sid': conversation.sid}
+    return {'token': token.to_jwt(),
+         'conversation_sid': conversation.sid}
+
+
 
 
 if __name__ == '__main__':
